2484c79e35057af816bbc9f7fcb4c69dce6a7d4c
Jack <jacktage123@yahoo.com>
2016-07-19 18:18:11 +0000

    lesson 7 first commit

---
+++ Lesson7_Assignment/script.js
@@ -0,0 +1,9 @@
+/*
+Program Name: Recipe Display Application
+Author: Jack Bartolucci
+Date: july 19, 2016
+Filename: script.js
+*/
+function display(event) {
+ $(event.currentTarget).next().fadeIn(“slow”);
+}
